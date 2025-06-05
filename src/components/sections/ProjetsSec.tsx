86c6a26325e1c8f4af04f6a5b824c09eac647371
gloire <gloirengayo@gmail.com>
2025-06-05 22:51:34 +0100

    finish section Home

--- src/components/sections/ProjetsSec.tsx
+++ src/components/sections/ProjetsSec.tsx
@@ -9,6 +9,10 @@ export default function ProjetsSec(){
           <h2 className="text-3xl md:text-5xl font-bold mb-8">Mes Projets</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {/* Ajoutez vos projets ici */}
+            <div className="card-border-gradient bg-white p-6 rounded-lg">
+              <h2 className="text-xl font-bold">Mon projet</h2>
+              <p>Un texte ici</p>
+            </div>
           </div>
         </div>
       </section>
